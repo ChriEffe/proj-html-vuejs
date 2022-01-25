1e9433ce2443adf7ab324b194e46257068299863
ChriEffe <christianferrazzano93@gmail.com>
2022-01-25 14:32:41 +0100

    add component popular

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -18,29 +18,7 @@
       </div>
     </div>
     <!-- popular dishes -->
-    <div class="popular container text-center">
-      <h2 class="fw-bold">Our Most Popular Dishes</h2>
-      <div class="row mb-4">
-        <div class="col-4">
-          <img src="../assets/img/skin-on-fries-400x571.jpg" alt="" />
-          <h3>Skin On Fries</h3>
-          <p>$3.00-$6.00</p>
-        </div>
-        <div class="col-4">
-          <img src="../assets/img/choco-cookie-frappe-400x571.jpg" alt="" />
-          <h3>Choko Cookie Frappe</h3>
-          <p>$4.99</p>
-        </div>
-        <div class="col-4">
-          <img src="../assets/img/donut-burger-400x571.jpg" alt="" />
-          <h3>The Donut Burger</h3>
-          <p>$6.99</p>
-        </div>
-      </div>
-      <button class="btn-menu btn rounded-pill fw-bold">
-        start your order <i class="fas fa-arrow-circle-right"></i>
-      </button>
-    </div>
+    <Popular />
     <!-- banner offer -->
     <div class="banner offer"></div>
     <!-- categories -->
@@ -53,15 +31,17 @@
           <h1 class="mb-5">
             <span>Ordering App</span>
           </h1>
-          <img
-            class="me-4"
-            src="../assets/img/app-store-badge-200x67.png"
-            alt="app store"
-          />
-          <img
-            src="../assets/img/play-store-badge-200x67.png"
-            alt="play store"
-          />
+          <a href="#"
+            ><img
+              class="me-4"
+              src="../assets/img/app-store-badge-200x67.png"
+              alt="app store"
+          /></a>
+          <a href="#"
+            ><img
+              src="../assets/img/play-store-badge-200x67.png"
+              alt="play store"
+          /></a>
         </div>
       </div>
     </div>
@@ -70,20 +50,20 @@
 
 <script>
 import Categories from "./Categories.vue";
+import Popular from "./Popular.vue";
 
 export default {
   name: "Main",
   components: {
     Categories,
+    Popular,
   },
 };
 </script>
 
 <style lang="scss" scoped>
 @import "../assets/scss/style.scss";
-.popular {
-  padding: 5em 0;
-}
+
 h2 {
   color: $black;
 }
@@ -97,16 +77,6 @@ h2 {
     color: $black;
     padding-right: 1em;
   }
-}
-p {
-  color: $button;
-}
-.btn-menu {
-  background-color: $button;
-  text-transform: uppercase;
-  color: $white;
-  font-size: 0.8em;
-  width: 16em;
 }
 .banner {
   height: 430px;
